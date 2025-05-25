9a35a725f49e6eebec18ea02537cba63e40d652d
XurxoMF <xurxolaxe@gmail.com>
2025-05-25 15:38:56 +0200

    Removed unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-  import { core } from "@tauri-apps/api";
-
   import { rustory } from "$lib/stores/rustory";
 
   import PageWrapper from "$lib/ui/layout/PageWrapper.svelte";
-  import { appDataDir, cacheDir, join, localDataDir } from "@tauri-apps/api/path";
-  import { onMount } from "svelte";
 
   rustory.mainWindow.breadcrumbs.segments = [];
 </script>
