7d6e538575a01589495cc39194d2c45460430920
XurxoMF <xurxolaxe@gmail.com>
2025-08-13 19:22:18 +0200

    Moved globals to the shared folder

--- src/renderer/src/lib/globals.ts
+++
@@ -1,4 +0,0 @@
-/**
- * The Rustory API URL to fetch things from.
- */
-export const API_BASE = import.meta.env.VITE_RUSTORY_API
---
+++ src/shared/globals.ts
@@ -0,0 +1,4 @@
+/**
+ * The Rustory API URL to fetch things from.
+ */
+export const API_BASE = 'https://api.rustory.xyz'
