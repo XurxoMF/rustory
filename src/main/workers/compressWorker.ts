ce2e4a990313179e7282e89c448219c07257d82a
XurxoMF <xurxolaxe@gmail.com>
2025-08-15 20:39:56 +0200

    Formatted some files

--- src/main/workers/compressWorker.ts
+++ src/main/workers/compressWorker.ts
@@ -1,12 +1,12 @@
-import { parentPort, workerData } from "worker_threads"
-import archiver from "archiver"
-import fse from "fs-extra"
-import { join } from "path"
+import { parentPort, workerData } from 'worker_threads'
+import archiver from 'archiver'
+import fse from 'fs-extra'
+import { join } from 'path'
 
 const { inputPath, outputPath, outputFileName, compressionLevel = 6 } = workerData
 
 const output = fse.createWriteStream(join(outputPath, outputFileName))
-const archive = archiver("zip", { zlib: { level: compressionLevel } })
+const archive = archiver('zip', { zlib: { level: compressionLevel } })
 
 let totalFiles = 0
 let lastReportedProgress = 0
@@ -30,24 +30,24 @@ try {
     fse.mkdirSync(outputPath, { recursive: true })
   }
 
-  archive.on("progress", ({ entries }) => {
+  archive.on('progress', ({ entries }) => {
     const progress = Math.floor((entries.processed / totalFiles) * 100)
     if (progress > lastReportedProgress) {
       lastReportedProgress = progress
       parentPort?.postMessage({
-        type: "progress",
+        type: 'progress',
         progress
       })
     }
   })
 
-  archive.on("error", (err) => {
-    parentPort?.postMessage({ type: "error", message: `Unexpected error: ${err}` })
+  archive.on('error', (err) => {
+    parentPort?.postMessage({ type: 'error', message: `Unexpected error: ${err}` })
   })
 
-  output.on("close", () => {
-    parentPort?.postMessage({ type: "progress", progress: 100 })
-    parentPort?.postMessage({ type: "finished" })
+  output.on('close', () => {
+    parentPort?.postMessage({ type: 'progress', progress: 100 })
+    parentPort?.postMessage({ type: 'finished' })
   })
 
   archive.pipe(output)
@@ -56,5 +56,5 @@ try {
 
   archive.finalize()
 } catch (err) {
-  parentPort?.postMessage({ type: "error", message: `Unexpected error: ${err}` })
+  parentPort?.postMessage({ type: 'error', message: `Unexpected error: ${err}` })
 }
