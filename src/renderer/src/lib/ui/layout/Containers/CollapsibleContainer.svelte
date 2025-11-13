63bfbe1381c1b0479c17d575c07b2f586f41dfac
XurxoMF <xurxolaxe@gmail.com>
2025-11-13 09:37:19 +0100

    More tweaks

--- src/renderer/src/lib/ui/layout/Containers/CollapsibleContainer.svelte
+++ src/renderer/src/lib/ui/layout/Containers/CollapsibleContainer.svelte
@@ -4,29 +4,14 @@
 
   import Icon from '@renderer/lib/ui/base/Icon.svelte'
 
-  const PADDING_CLASSES = {
-    0: ['p-0'],
-    1: ['p-1'],
-    2: ['p-2'],
-    3: ['p-3'],
-    4: ['p-4'],
-    5: ['p-5'],
-    6: ['p-6'],
-    7: ['p-7'],
-    8: ['p-8']
-  } as const
-
-  type PaddingTypes = keyof typeof PADDING_CLASSES
-
-  type CollapsibleContainerProps = Omit<WithoutChild<Collapsible.RootProps>, 'class'> & {
+  type CollapsibleContainerProps = WithoutChild<Collapsible.RootProps> & {
     headerContent: Snippet
-    padding?: PaddingTypes | undefined
   }
 
-  let { open = $bindable(false), ref = $bindable(null), headerContent, padding = 3, children, ...restProps }: CollapsibleContainerProps = $props()
+  let { open = $bindable(false), ref = $bindable(null), headerContent, children, class: classes, ...restProps }: CollapsibleContainerProps = $props()
 </script>
 
-<Collapsible.Root bind:open bind:ref class="w-full flex flex-col" {...restProps}>
+<Collapsible.Root bind:open bind:ref class={['w-full flex flex-col', classes]} {...restProps}>
   <Collapsible.Trigger
     class={[
       'w-full flex items-center justify-between p-2 rounded-t-md data-[state=closed]:rounded-b-md transition-[opacity,border-radius] duration-200',
@@ -50,8 +35,7 @@
       't-light:border-t-zinc-300',
       't-rust:border-t-rust-750',
       't-midnight:border-t-gray-750',
-      'data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up',
-      PADDING_CLASSES[padding]
+      'data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up'
     ]}
   >
     {@render children?.()}
