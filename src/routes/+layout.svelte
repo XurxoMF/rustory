00fd3ddb0722feb37f7f7f3ae2c371d4f9d70408
XurxoMF <xurxolaxe@gmail.com>
2026-04-03 13:49:19 +0200

    Added: Sidebar base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+
+	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
+
+	import SunIcon from '@tabler/icons-svelte/icons/sun';
+	import MoonIcon from '@tabler/icons-svelte/icons/moon';
 
 	import './layout.css';
 
-	import { page } from '$app/state';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+	import * as Separator from '$lib/components/ui/separator';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import * as Button from '$lib/components/ui/button';
+
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 
 	let { children } = $props();
@@ -11,7 +23,47 @@
 
 <ModeWatcher defaultMode="dark" />
 
-{@render children()}
+<Sidebar.Provider open={true}>
+	<AppSidebar />
+
+	<Sidebar.Inset>
+		<header class="flex shrink-0 flex-row items-center justify-between gap-2 p-2">
+			<div class="flex items-center gap-2">
+				<Sidebar.Trigger />
+
+				<Separator.Root orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+
+				<Breadcrumb.Root>
+					<Breadcrumb.List>
+						<Breadcrumb.Item class="hidden md:block">
+							<Breadcrumb.Link href={resolve('/')}>/</Breadcrumb.Link>
+						</Breadcrumb.Item>
+
+						<Breadcrumb.Separator class="hidden md:block" />
+
+						<Breadcrumb.Item>
+							<Breadcrumb.Page>Another Page</Breadcrumb.Page>
+						</Breadcrumb.Item>
+					</Breadcrumb.List>
+				</Breadcrumb.Root>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<Button.Root onclick={toggleMode} variant="ghost" size="icon-sm">
+					<SunIcon class="scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90" />
+					<MoonIcon
+						class="absolute scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button.Root>
+			</div>
+		</header>
+
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			{@render children()}
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
 
 <div style="display:none">
 	{#each locales as locale (locale)}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,7 @@
-<script lang="ts">
-	import { toggleMode } from 'mode-watcher';
-
-	import SunIcon from '@tabler/icons-svelte/icons/sun';
-	import MoonIcon from '@tabler/icons-svelte/icons/moon';
-
-	import { Button } from '$lib/components/ui/button/index.js';
-</script>
-
-<main>
-	<h1>Welcome to Rustory</h1>
-
-	<Button onclick={toggleMode} variant="outline" size="icon">
-		<SunIcon
-			class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
-		/>
-		<MoonIcon
-			class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
-		/>
-		<span class="sr-only">Toggle theme</span>
-	</Button>
-</main>
+<h1>Welcome to Rustory</h1>
+<h2>Welcome to Rustory</h2>
+<h3>Welcome to Rustory</h3>
+<h4>Welcome to Rustory</h4>
+<h5>Welcome to Rustory</h5>
+<h6>Welcome to Rustory</h6>
+<p>Welcome to Rustory</p>
