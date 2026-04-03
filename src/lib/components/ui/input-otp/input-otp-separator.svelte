3760a11ef6399c65f9245f71393e67e0f0f34188
XurxoMF <xurxolaxe@gmail.com>
2026-04-04 01:27:32 +0200

    Added: All the shadcn-svelte components

---
+++ src/lib/components/ui/input-otp/input-otp-separator.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+	import type { WithElementRef } from "$lib/utils.js";
+	import { cn } from "$lib/utils.js";
+	import { IconMinus } from "@tabler/icons-svelte";
+
+	let { ref = $bindable(null), class: className, children, ...restProps }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div
+	bind:this={ref}
+	data-slot="input-otp-separator"
+	role="separator"
+	class={cn("flex items-center [&_svg:not([class*='size-'])]:size-4", className)}
+	{...restProps}
+>
+	{#if children}
+		{@render children?.()}
+	{:else}
+		<IconMinus />
+	{/if}
+</div>
