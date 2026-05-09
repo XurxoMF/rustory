886e4020dadac0898fb9967c2cb36f32bedde2f0
XurxoMF <xurxolaxe@gmail.com>
2026-05-09 18:23:41 +0200

    Fixed: Sidebar having a collapsible section for no reason.

--- src/routes/app-sidebar-main.svelte
+++ src/routes/app-sidebar-main.svelte
@@ -4,26 +4,21 @@
 	import IconDeviceGamepad from "@tabler/icons-svelte/icons/device-gamepad";
 
 	import * as Sidebar from "$lib/components/ui/sidebar";
-	import * as Collapsible from "$lib/components/ui/collapsible";
 </script>
 
 <Sidebar.Group>
 	<Sidebar.GroupLabel>Vintage Story</Sidebar.GroupLabel>
 
 	<Sidebar.Menu>
-		<Collapsible.Root>
-			{#snippet child({ props })}
-				<Sidebar.MenuItem {...props}>
-					<Sidebar.MenuButton tooltipContent="Vintage Story Instances">
-						{#snippet child({ props })}
-							<a href={resolve("/vs-instances")} {...props}>
-								<IconDeviceGamepad />
-								<span>Instances</span>
-							</a>
-						{/snippet}
-					</Sidebar.MenuButton>
-				</Sidebar.MenuItem>
-			{/snippet}
-		</Collapsible.Root>
+		<Sidebar.MenuItem>
+			<Sidebar.MenuButton tooltipContent="Vintage Story Instances">
+				{#snippet child({ props })}
+					<a href={resolve("/vs-instances")} {...props}>
+						<IconDeviceGamepad />
+						<span>Instances</span>
+					</a>
+				{/snippet}
+			</Sidebar.MenuButton>
+		</Sidebar.MenuItem>
 	</Sidebar.Menu>
 </Sidebar.Group>
