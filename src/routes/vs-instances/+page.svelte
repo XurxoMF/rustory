774e8738a5fe6665e0e4a903040ef20059f7e6ce
XurxoMF <xurxolaxe@gmail.com>
2026-05-10 20:06:06 +0200

    Modified: Added empty list on Mods and Instances

--- src/routes/vs-instances/+page.svelte
+++ src/routes/vs-instances/+page.svelte
@@ -3,6 +3,7 @@
 	import { resolve } from "$app/paths";
 
 	import IconPlus from "@tabler/icons-svelte/icons/plus";
+	import IconDeviceGamepad from "@tabler/icons-svelte/icons/device-gamepad";
 
 	import { formatTime } from "$lib/utils";
 
@@ -18,6 +19,7 @@
 	import * as Spinner from "$lib/components/ui/spinner";
 	import * as Tooltip from "$lib/components/ui/tooltip";
 	import * as Table from "$lib/components/ui/table";
+	import * as Empty from "$lib/components/ui/empty";
 	import * as FloatingMenu from "$lib/components/ui/floating-menu";
 
 	import DeleteVSInstanceDialog from "$lib/components/vs-instances/delete-dialog.svelte";
@@ -31,101 +33,121 @@
 <Typo.Leading>Play and manage your Vintage Story Instances.</Typo.Leading>
 
 <!-- List of Vintage Story Instances -->
-<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-	{#each App.data.vsInstances as vsInstance (vsInstance.id)}
-		<Card.Root>
-			<Card.Header>
-				<Card.Title class="text-lg">{vsInstance.name}</Card.Title>
+{#if App.data.vsInstances.length === 0}
+	<Empty.Root>
+		<Empty.Header>
+			<Empty.Media variant="icon">
+				<IconDeviceGamepad />
+			</Empty.Media>
 
-				<Card.Description class="line-clamp-2">{vsInstance.description}</Card.Description>
+			<Empty.Title>No Vintage Story Instances found!</Empty.Title>
 
-				<Card.Action>
-					{@const vsVersion = App.data.vsVersions.find((v) => v.version === vsInstance.version)}
+			<Empty.Description>It looks like you've not created any Vintage Story Instances yet. Create one with the next button!</Empty.Description>
+		</Empty.Header>
 
-					{#if vsVersion === undefined}
-						<Badge.Root variant="destructive">Stopped</Badge.Root>
-					{:else if vsVersion.state === VSVersionState.INSTALLING}
-						<Badge.Root>
-							<Spinner.Root />
-							Installing...
-						</Badge.Root>
-					{:else if vsVersion.state === VSVersionState.DELETING || vsInstance.state === VSInstanceState.DELETING}
-						<Badge.Root variant="destructive">
-							<Spinner.Root />
-							Deleting...
-						</Badge.Root>
-					{:else if vsInstance.state === VSInstanceState.BACKUPING}
-						<Badge.Root>
-							<Spinner.Root />
-							Backing up...
-						</Badge.Root>
-					{:else if vsInstance.state === VSInstanceState.PLAYING_CLIENT}
-						<Badge.Root>
-							<Spinner.Root />
-							Running client...
-						</Badge.Root>
-					{:else if vsInstance.state === VSInstanceState.PLAYING_SERVER}
-						<Badge.Root>
-							<Spinner.Root />
-							Running server...
-						</Badge.Root>
-					{:else}
-						<Badge.Root variant="outline">Stopped</Badge.Root>
-					{/if}
-				</Card.Action>
-			</Card.Header>
+		<Empty.Content>
+			<Button.Root variant="outline">
+				<a href={resolve("/vs-instances/create")}> Create a Vintage Story Instance </a>
+			</Button.Root>
+		</Empty.Content>
+	</Empty.Root>
+{:else}
+	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+		{#each App.data.vsInstances as vsInstance (vsInstance.id)}
+			<Card.Root>
+				<Card.Header>
+					<Card.Title class="text-lg">{vsInstance.name}</Card.Title>
 
-			<Card.Content class="mt-auto">
-				<Table.Root>
-					<Table.Body>
-						<Table.Row>
-							<Table.Cell class="text-muted-foreground" align="left">Vintage Story Version</Table.Cell>
-							<Table.Cell class="font-bold" align="right">{vsInstance.version}</Table.Cell>
-						</Table.Row>
+					<Card.Description class="line-clamp-2">{vsInstance.description}</Card.Description>
 
-						<Table.Row>
-							<Table.Cell class="text-muted-foreground" align="left">Installed mods</Table.Cell>
-							<Table.Cell class="font-bold" align="right">{vsInstance.mods.length}</Table.Cell>
-						</Table.Row>
+					<Card.Action>
+						{@const vsVersion = App.data.vsVersions.find((v) => v.version === vsInstance.version)}
 
-						<Table.Row>
-							<Table.Cell class="text-muted-foreground" align="left">Last time played</Table.Cell>
-							<Table.Cell class="font-bold" align="right">
-								{vsInstance.lastTimePlayed === 0 ? "Never" : new Date(vsInstance.lastTimePlayed).toLocaleString(App.config.locale)}
-							</Table.Cell>
-						</Table.Row>
+						{#if vsVersion === undefined}
+							<Badge.Root variant="destructive">Stopped</Badge.Root>
+						{:else if vsVersion.state === VSVersionState.INSTALLING}
+							<Badge.Root>
+								<Spinner.Root />
+								Installing...
+							</Badge.Root>
+						{:else if vsVersion.state === VSVersionState.DELETING || vsInstance.state === VSInstanceState.DELETING}
+							<Badge.Root variant="destructive">
+								<Spinner.Root />
+								Deleting...
+							</Badge.Root>
+						{:else if vsInstance.state === VSInstanceState.BACKUPING}
+							<Badge.Root>
+								<Spinner.Root />
+								Backing up...
+							</Badge.Root>
+						{:else if vsInstance.state === VSInstanceState.PLAYING_CLIENT}
+							<Badge.Root>
+								<Spinner.Root />
+								Running client...
+							</Badge.Root>
+						{:else if vsInstance.state === VSInstanceState.PLAYING_SERVER}
+							<Badge.Root>
+								<Spinner.Root />
+								Running server...
+							</Badge.Root>
+						{:else}
+							<Badge.Root variant="outline">Stopped</Badge.Root>
+						{/if}
+					</Card.Action>
+				</Card.Header>
 
-						<Table.Row>
-							<Table.Cell class="text-muted-foreground" align="left">Total time played</Table.Cell>
-							<Table.Cell class="font-bold" align="right">
-								{`${formatTime(vsInstance.totalTimePlayed, { to: "minutes" })} minutes`}
-							</Table.Cell>
-						</Table.Row>
-					</Table.Body>
-				</Table.Root>
-			</Card.Content>
+				<Card.Content class="mt-auto">
+					<Table.Root>
+						<Table.Body>
+							<Table.Row>
+								<Table.Cell class="text-muted-foreground" align="left">Vintage Story Version</Table.Cell>
+								<Table.Cell class="font-bold" align="right">{vsInstance.version}</Table.Cell>
+							</Table.Row>
 
-			<Card.Footer class="flex justify-end gap-2">
-				<Button.Root class="flex-1" variant="destructive" onclick={() => (idDeletingInstance = vsInstance.id)}>Delete</Button.Root>
+							<Table.Row>
+								<Table.Cell class="text-muted-foreground" align="left">Installed mods</Table.Cell>
+								<Table.Cell class="font-bold" align="right">{vsInstance.mods.length}</Table.Cell>
+							</Table.Row>
 
-				<Button.Root class="flex-1" variant="outline" onclick={() => goto(resolve("/vs-instances/[slug]", { slug: vsInstance.id }))}>
-					Manage
-				</Button.Root>
+							<Table.Row>
+								<Table.Cell class="text-muted-foreground" align="left">Last time played</Table.Cell>
+								<Table.Cell class="font-bold" align="right">
+									{vsInstance.lastTimePlayed === 0 ? "Never" : new Date(vsInstance.lastTimePlayed).toLocaleString(App.config.locale)}
+								</Table.Cell>
+							</Table.Row>
 
-				<Button.Root class="flex-1" onclick={() => App.toaster.toast.info("Not implemented yet!")}>Play</Button.Root>
-			</Card.Footer>
+							<Table.Row>
+								<Table.Cell class="text-muted-foreground" align="left">Total time played</Table.Cell>
+								<Table.Cell class="font-bold" align="right">
+									{`${formatTime(vsInstance.totalTimePlayed, { to: "minutes" })} minutes`}
+								</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table.Root>
+				</Card.Content>
 
-			<DeleteVSInstanceDialog
-				open={idDeletingInstance === vsInstance.id}
-				onOpenChange={(open) => {
-					if (!open) idDeletingInstance = null;
-				}}
-				onSuccess={() => (idDeletingInstance = null)}
-				{vsInstance}
-			/>
-		</Card.Root>
-	{/each}
-</div>
+				<Card.Footer class="flex justify-end gap-2">
+					<Button.Root class="flex-1" variant="destructive" onclick={() => (idDeletingInstance = vsInstance.id)}>Delete</Button.Root>
+
+					<Button.Root class="flex-1" variant="outline" onclick={() => goto(resolve("/vs-instances/[slug]", { slug: vsInstance.id }))}>
+						Manage
+					</Button.Root>
+
+					<Button.Root class="flex-1" onclick={() => App.toaster.toast.info("Not implemented yet!")}>Play</Button.Root>
+				</Card.Footer>
+
+				<DeleteVSInstanceDialog
+					open={idDeletingInstance === vsInstance.id}
+					onOpenChange={(open) => {
+						if (!open) idDeletingInstance = null;
+					}}
+					onSuccess={() => (idDeletingInstance = null)}
+					{vsInstance}
+				/>
+			</Card.Root>
+		{/each}
+	</div>
+{/if}
 
 <FloatingMenu.Root>
 	<Tooltip.Root>
--- src/routes/vs-mods/+page.svelte
+++ src/routes/vs-mods/+page.svelte
@@ -3,6 +3,7 @@
 
 	import IconWorld from "@tabler/icons-svelte/icons/world";
 	import IconError404 from "@tabler/icons-svelte/icons/error-404";
+	import IconTool from "@tabler/icons-svelte/icons/tool";
 
 	import { App } from "$lib/classes/App.svelte";
 
@@ -16,9 +17,10 @@
 	import * as Combobox from "$lib/components/ui/combobox";
 	import * as Empty from "$lib/components/ui/empty";
 	import * as Button from "$lib/components/ui/button";
-
-	import ModDBApiBasicModCard from "./moddb-api-basic-mod-card.svelte";
-	import ModDBApiBasicModCardSkeleton from "./moddb-api-basic-mod-card-skeleton.svelte";
+	import * as Card from "$lib/components/ui/card";
+	import * as Badge from "$lib/components/ui/badge";
+	import * as Table from "$lib/components/ui/table";
+	import * as AspectRatio from "$lib/components/ui/aspect-ratio";
 
 	const modDBApiBasicModsPromise = ModDBApiBasicMod.fetchAll();
 
@@ -30,6 +32,7 @@
 <Typo.H1>Vintage Story Mods</Typo.H1>
 <Typo.Leading>View, install and manage Vintage Story Mods on your Vintage Story Instances.</Typo.Leading>
 
+<!-- Vintage Story Instance selector -->
 <Combobox.Root value={App.data.vsInstances[0]?.id} onchange={(v) => (vsInstance = App.data.vsInstances.find((i) => i.id === v))}>
 	<Combobox.Trigger>{vsInstance?.name || "Select a Vintage Story Instance..."}</Combobox.Trigger>
 
@@ -37,7 +40,7 @@
 		<Combobox.Input placeholder="Buscar..." />
 
 		<Combobox.List>
-			<Combobox.Empty>No Vintage Story Instances found.</Combobox.Empty>
+			<Combobox.Empty>No Vintage Story Instances found! Create one to install mods!</Combobox.Empty>
 
 			<Combobox.Group>
 				{#each App.data.vsInstances as i (i.id)}
@@ -48,19 +51,122 @@
 	</Combobox.Content>
 </Combobox.Root>
 
-<!-- List of Vintage Story Instances -->
+<!-- List of Vintage Story Mods -->
 {#await modDBApiBasicModsPromise}
 	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
 		{#each Array(20) as _, i (i)}
-			<ModDBApiBasicModCardSkeleton />
+			<Card.Root>
+				<Card.Header>
+					<Card.Title><Card.TitleSkeleton /></Card.Title>
+
+					<Card.Description>
+						<Card.DescriptionSkeleton />
+						<Card.DescriptionSkeleton />
+					</Card.Description>
+
+					<Card.Action>
+						<Badge.Skeleton />
+					</Card.Action>
+				</Card.Header>
+
+				<Card.Content class="mt-auto">
+					<AspectRatio.Skeleton ratio={3 / 2} class="overflow-hidden rounded-lg" />
+
+					<Table.Root class="mt-4">
+						<Table.Body>
+							{#each Array(4) as _, j (j)}
+								<Table.Row>
+									<Table.Cell align="left">
+										<Table.CellSkeleton />
+									</Table.Cell>
+
+									<Table.Cell align="right">
+										<Table.CellSkeleton />
+									</Table.Cell>
+								</Table.Row>
+							{/each}
+						</Table.Body>
+					</Table.Root>
+				</Card.Content>
+
+				<Card.Footer class="flex justify-end gap-2">
+					<Button.Skeleton class="flex-1" />
+
+					<Button.Skeleton class="flex-1" />
+				</Card.Footer>
+			</Card.Root>
 		{/each}
 	</div>
-{:then modDBApiModsOnList}
-	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-		{#each modDBApiModsOnList?.sort((a, b) => b.downloads - a.downloads).slice(0, 20) as modDBApiBasicMod (modDBApiBasicMod.modid)}
-			<ModDBApiBasicModCard {modDBApiBasicMod} />
-		{/each}
-	</div>
+{:then modDBApiBasicMods}
+	{#if modDBApiBasicMods.length === 0}
+		<Empty.Root>
+			<Empty.Header>
+				<Empty.Media variant="icon">
+					<IconTool />
+				</Empty.Media>
+
+				<Empty.Title>No Vintage Story Mods found!</Empty.Title>
+
+				<Empty.Description>Something is wrong in here... how the hell there are no mods?</Empty.Description>
+			</Empty.Header>
+
+			<Empty.Content>
+				<Button.Root variant="outline">
+					<a href={resolve("/")}> Return to home! </a>
+				</Button.Root>
+			</Empty.Content>
+		</Empty.Root>
+	{:else}
+		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+			{#each modDBApiBasicMods?.sort((a, b) => b.downloads - a.downloads).slice(0, 20) as modDBApiBasicMod (modDBApiBasicMod.modid)}
+				<Card.Root>
+					<Card.Header>
+						<Card.Title class="text-lg">{modDBApiBasicMod.name}</Card.Title>
+
+						<Card.Description class="line-clamp-2">{modDBApiBasicMod.summary}</Card.Description>
+
+						<Card.Action>
+							<Badge.Root variant="outline">{modDBApiBasicMod.side}</Badge.Root>
+						</Card.Action>
+					</Card.Header>
+
+					<Card.Content class="mt-auto">
+						<AspectRatio.Root ratio={3 / 2} class="overflow-hidden rounded-lg">
+							<img src={modDBApiBasicMod.logo} alt={`${modDBApiBasicMod.name} logo`} />
+						</AspectRatio.Root>
+
+						<Table.Root class="mt-4">
+							<Table.Body>
+								<Table.Row>
+									<Table.Cell class="text-muted-foreground" align="left">Author</Table.Cell>
+									<Table.Cell class="font-bold" align="right">{modDBApiBasicMod.author}</Table.Cell>
+								</Table.Row>
+
+								<Table.Row>
+									<Table.Cell class="text-muted-foreground" align="left">Follows</Table.Cell>
+									<Table.Cell class="font-bold" align="right">{modDBApiBasicMod.follows}</Table.Cell>
+								</Table.Row>
+
+								<Table.Row>
+									<Table.Cell class="text-muted-foreground" align="left">Downloads</Table.Cell>
+									<Table.Cell class="font-bold" align="right">{modDBApiBasicMod.downloads}</Table.Cell>
+								</Table.Row>
+
+								<Table.Row>
+									<Table.Cell class="text-muted-foreground" align="left">Total time played</Table.Cell>
+									<Table.Cell class="font-bold" align="right">{modDBApiBasicMod.comments}</Table.Cell>
+								</Table.Row>
+							</Table.Body>
+						</Table.Root>
+					</Card.Content>
+
+					<Card.Footer class="flex justify-end gap-2">
+						<Button.Root class="flex-1" variant="outline" onclick={() => App.toaster.toast.info("Not implemented yet!")}>View</Button.Root>
+					</Card.Footer>
+				</Card.Root>
+			{/each}
+		</div>
+	{/if}
 {:catch err}
 	{#if err instanceof AppError && err.code === AppErrorCodes.OFFLINE}
 		<Empty.Root>
