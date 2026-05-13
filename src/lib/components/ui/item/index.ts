25195136bc7ec5676cc70b0bd176f885380e082e
XurxoMF <xurxolaxe@gmail.com>
2026-05-13 12:51:26 +0200

    Added: Item Skeletons

--- src/lib/components/ui/item/index.ts
+++ src/lib/components/ui/item/index.ts
@@ -5,7 +5,9 @@ import Header, { type HeaderProps } from "./item-header.svelte";
 import Footer, { type FooterProps } from "./item-footer.svelte";
 import Content, { type ContentProps } from "./item-content.svelte";
 import Title, { type TitleProps } from "./item-title.svelte";
+import TitleSkeleton, { type TitleSkeletonProps } from "./item-title-skeleton.svelte";
 import Description, { type DescriptionProps } from "./item-description.svelte";
+import DescriptionSkeleton, { type DescriptionSkeletonProps } from "./item-description-skeleton.svelte";
 import Actions, { type ActionsProps } from "./item-actions.svelte";
 import Media, { type MediaProps, type MediaVariants, mediaVariants } from "./item-media.svelte";
 
@@ -17,7 +19,9 @@ export {
 	Footer,
 	Content,
 	Title,
+	TitleSkeleton,
 	Description,
+	DescriptionSkeleton,
 	Actions,
 	Media,
 	//
@@ -30,7 +34,9 @@ export {
 	type FooterProps,
 	type ContentProps,
 	type TitleProps,
+	type TitleSkeletonProps,
 	type DescriptionProps,
+	type DescriptionSkeletonProps,
 	type ActionsProps,
 	type MediaProps,
 	type MediaVariants,
